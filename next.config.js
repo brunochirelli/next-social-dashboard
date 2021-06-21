e1c4420b299a66bfa5aa6d4f600904436e54a028
brunochirelli <brunochirelli@gmail.com>
2021-06-21 17:14:51 -0300

    add bundle analyzer script and package

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withPlugins([[withBundleAnalyzer]])
